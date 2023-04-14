638c80e8cf70b58988b2379b687510c8bb56f13f
ChenWarren <72281162+ChenWarren@users.noreply.github.com>
2023-04-13 21:22:09 -0700

    site settings and sanity config

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from "sanity"
 import { deskTool } from "sanity/desk"
-// import schemas from "./sanity/schemas"
+import schemas from "./sanity/schemas"
+import siteInfo from "./settings/site-confit"
 
 const config = defineConfig({
-  title: 'Creatif Dev',
-  projectId: 'iig2glgh',
-  dataset: 'production',
-  apiVersion: '1.0',
-  basePath: '/admin',
+  title: siteInfo.title,
+  projectId: siteInfo.sanityProjectId,
+  dataset: siteInfo.sanityDataset,
+  apiVersion: siteInfo.version,
+  basePath: siteInfo.sanityBasePath,
   plugins: [deskTool()],
-  // schema: { types: schemas}
+  schema: { types: schemas}
 })
 
 export default config
---
+++ settings/site-confit.ts
@@ -0,0 +1,11 @@
+//Define basic site infomation
+
+const siteInfo = {
+  title: 'Creatif Dev', //site title
+  sanityProjectId: 'iig2glgh', //Sanity projectID
+  sanityDataset: 'production', //Sanity Dataset
+  version: '1.0', //Api version in sanity
+  sanityBasePath: '/admin', //Sanity base path
+}
+
+export default siteInfo
