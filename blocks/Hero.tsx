be02aaaabbb2a9c7544c0d703d1d634fdb0c2b9a
ChenWarren <72281162+ChenWarren@users.noreply.github.com>
2023-04-20 10:11:51 -0700

    update hero style

--- blocks/Hero.tsx
+++ blocks/Hero.tsx
@@ -1,23 +1,22 @@
-import Image from "next/image"
 import { Button } from "../components/Button"
 
 export default function Hero() {
 
   return (
     <>
-      <section className="flex flex-col h-full sm:px-6 justify-end items-center bg-[#5DD9BF] gap-8 ">
-        <div className="max-w-4xl mx-auto sm:px-4 pt-20">
-          <div className="sm:w-[800px] px-10 text-white flex flex-col items-center">
+      <section className="flex flex-col h-full justify-end items-center bg-[#5DD9BF]">
+        <div className="sm:max-w-4xl w-full mx-auto px-4 pt-20">
+          <div className="w-full md:px-10 text-white flex flex-col items-center mb-10">
             <h1 className="sm:text-5xl uppercase text-3xl mb-10">
               Innovate, Design, Transform 
             </h1>
-            <p className="text-lg mb-10 text-gray-100">
+            <p className="sm:text-lg mb-10 text-gray-100">
               Innovate, Design, Transform Discover the Power of Creatif.dev&apos;s Modern Web and App Solutions!
             </p>
             <Button title="Get Started"/>
           </div>
         </div>
-        <div className="sm:w-[800px] sm:h-[350px] w-full h-40 rounded-t-3xl bg-[url('/hero-main.png')] bg-cover bg-no-repeat"></div>
+        <div className="sm:max-w-[800px] sm:h-[350px] w-full h-40 rounded-t-3xl bg-[url('/hero-main.png')] bg-cover bg-no-repeat"></div>
       </section>
 
       <div className="bg-[#1B3A4D] w-full sm:h-[200px]">
