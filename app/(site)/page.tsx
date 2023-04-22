db65142bc3dcc3b5d1391d5ad196c17a1c5467b4
ChenWarren <72281162+ChenWarren@users.noreply.github.com>
2023-04-21 21:33:34 -0700

    update homepage section order

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -10,9 +10,9 @@ export default function Home() {
   return (
     <>
       <Hero/>
-      <HomeAbout/>
-      <HomeProjects/>
       <HomeBlog/>
+      <HomeProjects/>
+      <HomeAbout/>
       <Footer/>
     </>
   )
