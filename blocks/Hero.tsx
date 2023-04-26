d27873a1fae8fe6bdbfb6e78614119ce724b3b4c
ChenWarren <72281162+ChenWarren@users.noreply.github.com>
2023-04-25 22:03:01 -0700

    update hero bottom section

--- blocks/Hero.tsx
+++ blocks/Hero.tsx
@@ -8,10 +8,10 @@ export default function Hero() {
         <div className="sm:max-w-4xl w-full mx-auto px-4 pt-20">
           <div className="w-full md:px-10 text-white flex flex-col items-center mb-10">
             <h1 className="sm:text-5xl uppercase text-3xl text-center mb-10">
-              Innovate, Design, Transform 
+              Frontend, Backend, Web 
             </h1>
             <p className="max-w-[500px] sm:text-lg mb-10 text-gray-100 text-center">
-              Discover the Power of Creatif.dev&apos;s Modern Web and App Solutions!
+              Discover the Passion of Creatif.dev&apos;s Latest Web and App Solutions!
             </p>
             <Button title="Get Started"/>
           </div>
@@ -21,9 +21,9 @@ export default function Hero() {
 
       <div className="bg-[#1B3A4D] w-full sm:h-[200px]">
         <div className="max-w-4xl mx-auto sm:px-4 h-full flex flex-col sm:flex-row justify-between items-center">
-          <TextCard title="Frontend" description="Topics about Frontend"/>
-          <TextCard title="CMS" description="Topics about CMS"/>
-          <TextCard title="Node.js" description="Topics about Node.js"/>
+          <TextCard title="Next.js" description="Topics about Frontend development with Next.js and TypeScript"/>
+          <TextCard title="CMS" description="Topics about integrating popular CMS with frontend technologies"/>
+          <TextCard title="Cloud" description="Topics about everything about cloud and latest web technologies"/>
         </div>
       </div>
     </>
