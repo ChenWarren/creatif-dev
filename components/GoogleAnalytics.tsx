a7c40169d28ff589ca701492e20f95757fcd339e
ChenWarren <72281162+ChenWarren@users.noreply.github.com>
2023-04-29 11:13:05 -0700

    update google analytics

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -1,52 +1,38 @@
 "use client"
 
-import { GTM_ID, pageview } from "@/lib"
+import { GTM_ID } from "@/lib"
 import Script from "next/script"
-import { useEffect } from "react"
-import { usePathname, useSearchParams } from "next/navigation"
 
 
 export const GoogleAnalytics = () => {
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
-
-  useEffect(() => {
-    if(pathname) {
-      pageview(pathname)
-    }
-  }, [pathname, searchParams])
 
   return (
     <>
-      <noscript>
-        <iframe 
-          src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`}
-          height={0}
-          width={0}
-          style={{display: 'none', visibility: 'hidden'}}
-        />
-      </noscript>
       <Script
-        id="gtm-script"
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`}
+      />
+      <Script
+        id="google-script"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtag/js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer', '${GTM_ID}');
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('consent', 'default', {
+              'analytics_storage': 'denied'
+          });
+          
+          gtag('config', '${GTM_ID}', {
+              page_path: window.location.pathname,
+          });
           `,
         }}
       />
-      {/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-6XH4SQQ5PW"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-6XH4SQQ5PW');
-      </script> */}
     </>
   )
 }
+
+// https://gaudion.dev/blog/setup-google-analytics-with-gdpr-compliant-cookie-consent-in-nextjs13
